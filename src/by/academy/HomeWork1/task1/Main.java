ec19fc733635e879d1b388acb74028c912404996
Anna Zenevich <annettka.gentille@gmail.com>
2020-03-22 14:59:07 +0300

    first commit

---
+++ src/by/academy/HomeWork1/task1/Main.java
@@ -0,0 +1,11 @@
+package by.academy.HomeWork1.task1;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        CalculatorWithOperator calc = new CalculatorWithOperator();
+        double x = calc.getSum(4.1, calc.getSum(calc.getProduct(15, 7), calc.raiseToPower(calc.getQuotient(28, 5), 2)));
+        System.out.printf("%.3f %n", x);
+    }
+}
