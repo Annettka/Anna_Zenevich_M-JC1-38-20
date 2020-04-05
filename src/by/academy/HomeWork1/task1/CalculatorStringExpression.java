7d79822e0072c5f3b3bdc19253560cd4b1604e59
Anna Zenevich <annettka.gentille@gmail.com>
2020-04-05 19:18:04 +0300

    Calculator String Expression

--- src/by/academy/HomeWork1/task1/CalculatorStringExpression.java
+++ src/by/academy/HomeWork1/task1/CalculatorStringExpression.java
@@ -2,23 +2,19 @@ package by.academy.HomeWork1.task1;
 
 public class CalculatorStringExpression {
 
-    final int NONE = 0;         //  FAIL
-    final int DELIMITER = 1;    //  Разделитель(+-*/^=, ")", "(" )
-    final int NUMBER = 3;       //  Число
+    final int NONE = 0;
+    final int DELIMITER = 1;
+    final int NUMBER = 3;
+    final int SYNTAXERROR = 0;
+    final int UNBALPARENS = 1; //not closed parentheses
+    final int NOEXP = 2;       //no expr
+    final int DIVBYZERO = 3;   //div by zero
+    final String EOF = "\0";   //end of expr
 
-    //  Объявление констант синтаксических ошибок
-    final int SYNTAXERROR = 0;  //  Синтаксическая ошибка (10 + 5 6 / 1)
-    final int UNBALPARENS = 1;  //  Несовпадение количества открытых и закрытых скобок
-    final int NOEXP = 2;        //  Отсутствует выражение при запуске анализатора
-    final int DIVBYZERO = 3;    //  Ошибка деления на ноль
-
-    //  Лексема, определяющая конец выражения
-    final String EOF = "\0";
-
-    private String exp;     //  Ссылка на строку с выражением
-    private int currIndex;     //  Текущий индекс в выражении
-    private String token;   //  Сохранение текущей лексемы
-    private int tokType;    //  Сохранение типа лексемы
+    private String exp;
+    private int currIndex;
+    private String token;
+    private int tokType;
 
     private void getToken() {
         tokType = NONE;
@@ -33,7 +29,7 @@ public class CalculatorStringExpression {
         }
         if (currIndex == exp.length()) {
             token = EOF;
-           return;
+            return;
         }
         if (isDelim(exp.charAt(currIndex))) {
             token += exp.charAt(currIndex);
@@ -43,8 +39,9 @@ public class CalculatorStringExpression {
             while (!isDelim(exp.charAt(currIndex))) {
                 token += exp.charAt(currIndex);
                 currIndex++;
-                if (currIndex >= exp.length())
+                if (currIndex >= exp.length()) {
                     break;
+                }
             }
             tokType = NUMBER;
         } else {
@@ -57,7 +54,7 @@ public class CalculatorStringExpression {
         return (("+-/*^=()".indexOf(charAt)) != -1) ? true : false;
     }
 
-    //  Точка входа
+    //  Enter point
     public double evaluate(String expr) throws Exception {
         double result;
         exp = expr;
@@ -101,8 +98,7 @@ public class CalculatorStringExpression {
         double partialResult;
 
         result = evalRaiseToPow();
-        while ((op = token.charAt(0)) == '*' ||
-                op == '/' | op == '%') {
+        while ((op = token.charAt(0)) == '*' || op == '/') {
             getToken();
             partialResult = evalRaiseToPow();
             switch (op) {
@@ -110,8 +106,9 @@ public class CalculatorStringExpression {
                     result *= partialResult;
                     break;
                 case '/':
-                    if (partialResult == 0.0)
+                    if (partialResult == 0.0) {
                         handleErr(DIVBYZERO);
+                    }
                     result /= partialResult;
                     break;
             }
@@ -132,8 +129,9 @@ public class CalculatorStringExpression {
             if (partialResult == 0.0) {
                 result = 1.0;
             } else
-                for (t = (int) partialResult - 1; t > 0; t--)
+                for (t = (int) partialResult - 1; t > 0; t--) {
                     result *= exp;
+                }
         }
         return result;
     }
@@ -148,8 +146,9 @@ public class CalculatorStringExpression {
             getToken();
         }
         result = evalParentExp();
-        if (op.equals("-"))
+        if (op.equals("-")) {
             result = -result;
+        }
         return result;
     }
 
@@ -159,11 +158,13 @@ public class CalculatorStringExpression {
         if (token.equals("(")) {
             getToken();
             result = evalPlusMinus();
-            if (!token.equals(")"))
+            if (!token.equals(")")) {
                 handleErr(UNBALPARENS);
+            }
             getToken();
-        } else
+        } else {
             result = getValue();
+        }
         return result;
     }
 
@@ -185,105 +186,9 @@ public class CalculatorStringExpression {
         return result;
     }
 
-    //  Кинуть ошибку
     private void handleErr(int nOEXP2) throws Exception {
         String[] err = {"Syntax error", "Unbalanced Parentheses",
                 "No Expression Present", "Division by zero"};
-
         throw new Exception(err[nOEXP2]);
     }
-
-    /*    private double currentState;
-        private String restExpression;
-
-        public void getResult(double v, String r) {
-            this.currentState = v;
-            this.restExpression = r;
-
-            4.1 + 15 * 7 + (28 / 5) ^ 2
-        }*/
-
-/*
-
-    public static double eval(String str) {
-        return new Object() {
-            int pos = -1, ch;
-
-            void nextChar() {
-                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
-            }
-
-            boolean eat(int charToEat) {
-                while (ch == ' ') nextChar();
-                if (ch == charToEat) {
-                    nextChar();
-                    return true;
-                }
-                return false;
-            }
-
-            double parse() {
-                nextChar();
-                double x = parseExpression();
-                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char) ch);
-                return x;
-            }
-
-            // Grammar:
-            // expression = term | expression `+` term | expression `-` term
-            // term = factor | term `*` factor | term `/` factor
-            // factor = `+` factor | `-` factor | `(` expression `)`
-            //        | number | functionName factor | factor `^` factor
-
-            double parseExpression() {
-                double x = parseTerm();
-                for (; ; ) {
-                    if (eat('+')) x += parseTerm(); // addition
-                    else if (eat('-')) x -= parseTerm(); // subtraction
-                    else return x;
-                }
-            }
-
-            double parseTerm() {
-                double x = parseFactor();
-                for (; ; ) {
-                    if (eat('*')) x *= parseFactor(); // multiplication
-                    else if (eat('/')) x /= parseFactor(); // division
-                    else return x;
-                }
-            }
-
-            double parseFactor() {
-                if (eat('+')) return parseFactor(); // unary plus
-                if (eat('-')) return -parseFactor(); // unary minus
-
-                double x;
-                int startPos = this.pos;
-                if (eat('(')) { // parentheses
-                    x = parseExpression();
-                    eat(')');
-                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
-                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
-                    x = Double.parseDouble(str.substring(startPos, this.pos));
-                } else if (ch >= 'a' && ch <= 'z') { // functions
-                    while (ch >= 'a' && ch <= 'z') nextChar();
-                    String func = str.substring(startPos, this.pos);
-                    x = parseFactor();
-                    if (func.equals("sqrt")) x = Math.sqrt(x);
-                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-                    else throw new RuntimeException("Unknown function: " + func);
-                } else {
-                    throw new RuntimeException("Unexpected: " + (char) ch);
-                }
-
-                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
-
-                return x;
-            }
-        }.parse();
-    }*/
-
-
 }
