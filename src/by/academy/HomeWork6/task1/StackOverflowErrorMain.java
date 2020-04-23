b337494c7143c47697ab446c51b1f8b27ba87cba
Annettka <annettka.gentille@gmail.com>
2020-04-23 18:19:23 +0300

    Exceptions and Errors

---
+++ src/by/academy/HomeWork6/task1/StackOverflowErrorMain.java
@@ -0,0 +1,19 @@
+package by.academy.HomeWork6.task1;
+
+public class StackOverflowErrorMain {
+
+    public static void main(String[] args) {
+
+        try {
+            Man man = new Man();
+        }catch (StackOverflowError e){
+            e.getMessage();
+            System.err.println("Ой! Переполнение стека при создании объекта класса Man");
+        }
+    }
+}
+
+class Man{
+    String name;
+    Man man = new Man();
+}
