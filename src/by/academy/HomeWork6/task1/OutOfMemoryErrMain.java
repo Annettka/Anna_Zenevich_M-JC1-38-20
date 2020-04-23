1f1e18bcd836adb045621d9d01c659bafd529747
Annettka <annettka.gentille@gmail.com>
2020-04-23 16:48:39 +0300

    Exceptions and OutOfMemoryError

---
+++ src/by/academy/HomeWork6/task1/OutOfMemoryErrMain.java
@@ -0,0 +1,23 @@
+package by.academy.HomeWork6.task1;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class OutOfMemoryErrMain {
+    public static void main(String[] args) {
+        ArrayList<Object> objects = new ArrayList<>();
+        try {
+            for (;;) {
+                objects.add(new Object());
+            }
+        }catch (OutOfMemoryError e){
+            objects.clear();
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Ошибка добавления Object в ArrayList<Object> objects.\nЗадайте размер ArrayList:");
+            objects.ensureCapacity(scan.nextInt());
+        }
+        System.out.println(Arrays.toString(objects.toArray()));
+    }
+}
